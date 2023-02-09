bddc3fa94de6774a47ea7605188fadf6a1419f56
Jorge Torres <jtorres1@ucol.mx>
2023-02-08 21:40:27 -0600

    JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,24 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
-const User=require('../models/userModel')
 
-const registrarUser=asyncHandler(async(req,res)=>{
-    const {name,email,password}=req.body
+const User = require('../models/userModel')
+
+const registrarUser = asyncHandler(async (req, res) => {
+
+    const { name, email, password } = req.body
+
     if (!name || !email || !password) {
         res.status(400)
         throw new Error('Faltan datos, favor de verificar')
     }
-        const userExiste = await User.findOne({ email })
+
+    const userExiste = await User.findOne({ email })
     if (userExiste) {
         res.status(400)
         throw new Error('Ese usuario ya existe')
     }
+
     //Hash password
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
@@ -35,16 +40,44 @@ const registrarUser=asyncHandler(async(req,res)=>{
         throw new Error('No se pudo agregar el usuario')
     }
 
-    
 })
 
-const loginUser=(req,res)=>{
-    res.json({message:'Logear usuario'})
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Credenciales incorrectas')
+    }
+
+})
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
 }
-const dataUser=(req,res)=>{
-    res.json({message:'Data usuario'})
-}
-module.exports={
+
+const dataUser = asyncHandler(async (req, res) => {
+    const { _id, name, email } = req.user
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
+
+})
+
+module.exports = {
     registrarUser,
     loginUser,
     dataUser
